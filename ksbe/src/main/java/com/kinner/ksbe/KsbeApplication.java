357e2456106440bf3fffd0da7db67a176c0c58de
niezefei <niezefei@rong360.com>
2020-04-19 10:30:04 +0800

    [Add]-repository files

---
+++ ksbe/src/main/java/com/kinner/ksbe/KsbeApplication.java
@@ -0,0 +1,13 @@
+package com.kinner.ksbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsbeApplication.class, args);
+    }
+
+}
