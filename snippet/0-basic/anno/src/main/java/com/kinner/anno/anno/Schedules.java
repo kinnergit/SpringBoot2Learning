b124933fad0ac3531468aa4f985f1d43d638ec6c
niezefei <niezefei@rong360.com>
2020-05-19 15:41:38 +0800

    [Update]-Schedules.java

--- snippet/0-basic/anno/src/main/java/com/kinner/anno/anno/Schedules.java
+++ snippet/0-basic/anno/src/main/java/com/kinner/anno/anno/Schedules.java
@@ -3,7 +3,7 @@ package com.kinner.anno.anno;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Schedules {
     Schedule[] value();
 }
