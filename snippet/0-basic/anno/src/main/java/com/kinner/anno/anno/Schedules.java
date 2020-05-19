42558fb3c6eb2a5c6c92ecc420ab5981358e7068
niezefei <niezefei@rong360.com>
2020-05-19 15:40:31 +0800

    [Update]-Repeatable Annotation

--- snippet/0-basic/anno/src/main/java/com/kinner/anno/anno/Schedules.java
+++ snippet/0-basic/anno/src/main/java/com/kinner/anno/anno/Schedules.java
@@ -2,9 +2,8 @@ package com.kinner.anno.anno;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Schedules {
     Schedule[] value();
 }
